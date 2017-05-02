c30efedb5f3005e661897e5a45ffd94d389b0dc6
Bryan Rosander <brosander@apache.org>
2017-05-02 09:46:47 -0400

    allow cmake flags, source volume

--- Dockerfiles/ubuntu/xenial/build/README.md
+++ Dockerfiles/ubuntu/xenial/build/README.md
@@ -7,7 +7,7 @@ Make an output directory
 ```
 mkdir build
 ```
-Run image with pr number
+Run image with pr number and debug info
 ```
-docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build PR_NUMBER
+docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build -c -DCMAKE_BUILD_TYPE=RelWithDebInfo -p PR_NUMBER
 ```
--- Dockerfiles/ubuntu/xenial/build/start.sh
+++ Dockerfiles/ubuntu/xenial/build/start.sh
@@ -2,13 +2,52 @@
 
 set -e
 
-mkdir ~/nifi-minifi-cpp
-cd ~/nifi-minifi-cpp
-git init
-git remote add origin https://github.com/apache/nifi-minifi-cpp.git
-git fetch origin pull/"$1"/head:pr"$1" && git checkout pr"$1"
-cd /build
-cmake /home/minifi/nifi-minifi-cpp
-make
-make test
-make package
+CMAKE_ARGS=""
+PR=""
+
+function printUsageAndExit() {
+  echo "usage: $0 [-c CMAKE_ARGS] [-p PR_NUMBER]"
+  echo "       -h or --help          print this message and exit"
+  echo "       -c or --cmake         cmake arg"
+  echo "       -p or --pr            pr number to build"
+  exit 1
+}
+
+# see https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash/14203146#14203146
+while [[ $# -ge 1 ]]; do
+  key="$1"
+  case $key in
+    -c|--cmake)
+    CMAKE_ARGS="$CMAKE_ARGS $2"
+    shift
+    ;;
+    -p|--pr)
+    PR="$2"
+    shift
+    ;;
+    -h|--help)
+    printUsageAndExit
+    ;;
+    *)
+    echo "Unknown option: $key"
+    echo
+    printUsageAndExit
+    ;;
+  esac
+  shift
+done
+
+if [ -z "$PR" ]; then
+  bash
+else
+  mkdir /source/nifi-minifi-cpp
+  cd /source/nifi-minifi-cpp
+  git init
+  git remote add origin https://github.com/apache/nifi-minifi-cpp.git
+  git fetch origin pull/"$PR"/head:pr"$PR" && git checkout pr"$PR"
+  cd /build
+  cmake $CMAKE_ARGS /source/nifi-minifi-cpp
+  make
+  make test
+  make package
+fi
