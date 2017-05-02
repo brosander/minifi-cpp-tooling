d3da25e97726413369d93bf1467488d6c771ca43
Bryan Rosander <brosander@apache.org>
2017-05-02 13:01:12 -0400

    Improving xenial start.sh

--- Dockerfiles/ubuntu/xenial/build/README.md
+++ Dockerfiles/ubuntu/xenial/build/README.md
@@ -1,13 +1,32 @@
-To build PR:
+This is a docker container intended to make building and testing Apache MiNiFi C++ easier.
+------------------------------------------------------------------------------------------
 Build image (one time)
 ```
 docker build -t minifi-cpp-xenial-build .
 ```
+```
+usage: docker run -ti [--rm] minifi-cpp-xenial-build [-r REPOSITORY] [-b BRANCH] [-p PR_NUMBER] [-c COMMAND_LINE]
+       -h or --help          print this message and exit
+       -r or --repository    repository (default: https://github.com/apache/nifi-minifi-cpp.git)
+       -b or --branch        branch to build
+       -p or --pr            pr number to build
+       -c or --commandLine   command line to execute (default: "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo /source/nifi-minifi-cpp && make && make test && make package")
+```
+
+To build PR:
 Make an output directory
 ```
 mkdir build
 ```
-Run image with pr number and debug info
+Run image with pr number
 ```
-docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build -c -DCMAKE_BUILD_TYPE=RelWithDebInfo -p PR_NUMBER
+docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build -p PR_NUMBER
+```
+Run image with branch
+```
+docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build -b BRANCH
+```
+Run image with branch and pr number
+```
+docker run -ti --rm -v "$(pwd)/build:/build" minifi-cpp-xenial-build -b BRANCH -p PR_NUMBER
 ```
--- Dockerfiles/ubuntu/xenial/build/start.sh
+++ Dockerfiles/ubuntu/xenial/build/start.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-CMAKE_ARGS=""
+COMMAND="cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo /source/nifi-minifi-cpp && make && make test && make package"
 PR=""
+BRANCH=""
+REPOSITORY="https://github.com/apache/nifi-minifi-cpp.git"
 
 function printUsageAndExit() {
-  echo "usage: $0 [-c CMAKE_ARGS] [-p PR_NUMBER]"
+  echo "usage: docker run -ti [--rm] minifi-cpp-xenial-build [-r REPOSITORY] [-b BRANCH] [-p PR_NUMBER] [-c COMMAND_LINE]"
   echo "       -h or --help          print this message and exit"
-  echo "       -c or --cmake         cmake arg"
+  echo "       -r or --repository    repository (default: $REPOSITORY)"
+  echo "       -b or --branch        branch to build"
   echo "       -p or --pr            pr number to build"
+  echo "       -c or --commandLine   command line to execute (default: \"$COMMAND\")"
   exit 1
 }
 
@@ -17,12 +21,20 @@ function printUsageAndExit() {
 while [[ $# -ge 1 ]]; do
   key="$1"
   case $key in
-    -c|--cmake)
-    CMAKE_ARGS="$CMAKE_ARGS $2"
+    -r|--repository)
+    REPOSITORY="$2"
+    shift
+    ;;
+    -b|--branch)
+    BRANCH="$2"
     shift
     ;;
     -p|--pr)
     PR="$2"
+    shift
+    ;;
+    -c|--commandLine)
+    COMMAND="$2"
     shift
     ;;
     -h|--help)
@@ -37,17 +49,23 @@ while [[ $# -ge 1 ]]; do
   shift
 done
 
-if [ -z "$PR" ]; then
-  bash
-else
+if [ ! -e "/source/nifi-minifi-cpp" ]; then
   mkdir /source/nifi-minifi-cpp
   cd /source/nifi-minifi-cpp
   git init
-  git remote add origin https://github.com/apache/nifi-minifi-cpp.git
-  git fetch origin pull/"$PR"/head:pr"$PR" && git checkout pr"$PR"
-  cd /build
-  cmake $CMAKE_ARGS /source/nifi-minifi-cpp
-  make
-  make test
-  make package
+  git checkout -B temporary-branch-that-shouldnt-exist
+  git remote add origin "$REPOSITORY"
 fi
+
+if [ -n "$BRANCH" ]; then
+  git fetch origin "$BRANCH:$BRANCH" && git checkout "$BRANCH"
+  if [ -n "$PR" ]; then
+    git pull origin pull/"$PR"/head
+  fi
+elif [ -n "$PR" ]; then
+  git fetch origin pull/"$PR"/head:pr"$PR" && git checkout pr"$PR"
+fi
+
+cd /build
+
+bash -c "$COMMAND"
